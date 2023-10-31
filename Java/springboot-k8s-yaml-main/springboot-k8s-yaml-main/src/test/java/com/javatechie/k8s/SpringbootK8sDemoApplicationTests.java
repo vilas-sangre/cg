fefc459fa3b6dec7864d321aa4ff3096a4cc2911
Vilas Sangre <vilas.sangre@eaton.com>
2023-10-31 11:51:41 +0530

    Spring Boot Application in K8s

---
+++ Java/springboot-k8s-yaml-main/springboot-k8s-yaml-main/src/test/java/com/javatechie/k8s/SpringbootK8sDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.javatechie.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SpringbootK8sDemoApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
