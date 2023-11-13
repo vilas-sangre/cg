5d702611ad868383ddd14c551cc61741c3790fd0
Vilas Sangre <vilas.sangre@eaton.com>
2023-11-13 14:14:57 +0530

    security project added

---
+++ Java/CGProject/src/com/cg/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.cg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Java/springboot-crud-k8s-main/springboot-crud-k8s-main/src/main/java/com/cg/crud/SpringbootCrudK8sExampleApplication.java
+++ Java/springboot-crud-k8s-main/springboot-crud-k8s-main/src/main/java/com/cg/crud/SpringbootCrudK8sExampleApplication.java
@@ -2,8 +2,10 @@ package com.cg.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootCrudK8sExampleApplication {
 
 	public static void main(String[] args) {
